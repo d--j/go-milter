d73d8ffb8e97443238a0fb5f18d2c370a39750b8
Masafumi Yamamoto <m-yamamoto@sakura.ad.jp>
2023-03-27 14:55:22 +0900

    Fixed IPv6 address parsing to handle IPv6: prefix

--- session.go
+++ session.go
@@ -174,6 +174,8 @@ func (m *serverSession) Process(msg *wire.Message) (*Response, error) {
 				if addr != nil {
 					address = addr.String()
 				}
+			} else if strings.HasPrefix(address, "IPv6:") {
+				addr = net.ParseIP(address[5:])
 			} else {
 				addr = net.ParseIP(address)
 			}
